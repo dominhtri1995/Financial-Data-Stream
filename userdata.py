b9da1330fec58b9f14f3e990af31d8492cb2fc8c
Do Minh Tri <TriDo@Dos-MacBook-Pro.local>
2017-03-30 22:38:56 -0400

    add requests POST to firebase server

--- userdata.py
+++ userdata.py
@@ -1,23 +1,21 @@
 import socket
-import pyrebase
 import time
 import datetime
+import requests
+import json
 
-config = {
-  "apiKey": "AIzaSyBoD1M_77br2rFsIGj_0mQ6qYqT70_gCD",
-  "authDomain": "s3463625.firebaseapp.com",
-  "databaseURL": "https://s3463625.firebaseio.com/",
-  "storageBucket": "s3463625.appspot.com"
-}
-
-firebase = pyrebase.initialize_app(config)
-
+# config = {
+#   "apiKey": "",
+#   "authDomain": "s3463625.firebaseapp.com",
+#   "databaseURL": "https://s3463625.firebaseio.com/",
+#   "storageBucket": "s3463625.appspot.com"
+# }
 
 def get_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect(("8.8.8.8", 80))
     try:
-        push_firebase("ip",s.getsockname()[0])
+        push_firebase("ip", s.getsockname()[0])
     except:
        pass
     s.close()
@@ -25,11 +23,11 @@ def get_ip():
 def get_computer_name():
     return socket.gethostname()
 
+
 def push_firebase(name,item):
-    db = firebase.database()
     offset = time.timezone if (time.localtime().tm_isdst == 0) else time.altzone
 
     data = {name:item,"time":datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S') \
             , "gmt":str(offset / 60 / 60 * -1),"computer_name":get_computer_name()}
 
-    db.child("IP").push(data)
+    requests.post("https://s3463625.firebaseio.com/ip.json",data=json.dumps(data))
